c6031b725e704308303a11adbb4ab58b9e0777e5
HoussamAW <houssamabdoulwahab@gmail.com>
2026-04-02 09:11:04 +0200

    add immersive game loop with targets and score

--- HogwartGame/ImmersiveView.swift
+++ HogwartGame/ImmersiveView.swift
@@ -10,18 +10,22 @@ import RealityKit
 
 struct ImmersiveView: View {
     @State private var game = GameState()
+    @State private var gameRoot = Entity()
 
     var body: some View {
         ZStack {
             RealityView { content in
+                gameRoot.name = "GameRoot"
+                content.add(gameRoot)
+
                 if let chamber = try? await ModelEntity(named: "Chamber") {
+                    chamber.scale = [3,3,3]
                     chamber.position = [0, -1.5, -10]
                     content.add(chamber)
                 }
 
-                let gameRoot = Entity()
-                gameRoot.name = "GameRoot"
-                content.add(gameRoot)
+                startTimer(game: game)
+                startSpawning(into: gameRoot, game: game)
             }
 
             VStack {
@@ -31,9 +35,32 @@ struct ImmersiveView: View {
                     Text("Temps: \(game.timeRemaining)")
                 }
                 .padding()
+
                 Spacer()
+                if game.isGameOver {
+                    VStack(spacing: 12) {
+                        Text("Game Over")
+                            .font(.largeTitle)
+                        Text("Score final \(game.score)")
+                    }.padding()
+                        .glassBackgroundEffect()
+                }
             }
-        }
+        }.gesture(
+            TapGesture()
+                .targetedToAnyEntity()
+                .onEnded{ value in
+                    handleTap(on: value.entity)
+                }
+        )
         .preferredSurroundingsEffect(.colorMultiply(.black))
     }
-}
+    private func handleTap(on entity: Entity) {
+            guard !game.isGameOver else { return }
+            guard entity.components[TargetComponent.self] != nil else { return }
+
+            entity.removeFromParent()
+            game.score += 1
+        }
+    }
+
